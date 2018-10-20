5cac6ff53c63f44dda321d89a17980a829984dd7
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-20 16:45:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('message', async message => { // Alpha Codes Server.
     if(message.author.bot) return;
     if(message.channel.type === 'dm') return;
  
-    var prefix = '#'; //<==== تقدر تغير البرفكس
+    var prefix = '$'; //<==== تقدر تغير البرفكس
     var args = message.content.toLowerCase().split(" "); // Alpha Codes Server.
     var command = args[0];
  
@@ -261,7 +261,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`لا الله الا الله |By:LooZ`,"http://twitch.tv/S-F")
+client.user.setGame(`لا الله الا الله `,"http://twitch.tv/S-F")
 client.user.setStatus("online")
  
 });
